500839da109a067f85dda31b21a7d04b8a13dfe1
Danny Drysdale <126314303+Daniel-Drysdale@users.noreply.github.com>
2025-03-20 12:44:58 -0400

    Updated DataDisplay - Type
    Example Data Type of what will likely be used and requested from InfluxDB

--- src/components/DataDisplay.tsx
+++ src/components/DataDisplay.tsx
@@ -1,5 +1,11 @@
 //Displays the data - currently a test for components
 
+type SolarCarData = {
+  example1: number;
+  example2: string;
+  example3: string;
+};
+
 function DataDisplay() {
   return <>Component Test</>;
 }
