0fc0521a5aae50373cf81b296af14227a2fa7537
Danny Drysdale <126314303+Daniel-Drysdale@users.noreply.github.com>
2025-03-24 17:28:02 -0400

    Basic Layout Completed
    Basic Layout Completed - waiting on integration with InfluxDB

--- src/components/DataDisplay.tsx
+++ src/components/DataDisplay.tsx
@@ -1,13 +1,50 @@
-//Displays the data - currently a test for components
+import { useEffect, useState } from "react";
 
 type SolarCarData = {
-  example1: number;
-  example2: string;
-  example3: string;
+  userId: number;
+  id: number;
+  title: string;
 };
 
-function DataDisplay() {
-  return <>Component Test</>;
-}
+const DataDisplay = () => {
+  const BASE_URL = import.meta.env.VITE_BASE_DB_URL;
+
+  const [Data, setData] = useState<SolarCarData>({
+    id: 0,
+    userId: 0,
+    title: "(Defaults) Some statistics there",
+  });
+
+  useEffect(() => {
+    const fetch_data = async () => {
+      try {
+        const DBresponse = await fetch(BASE_URL, { method: "GET" });
+
+        if (!DBresponse.ok) {
+          throw new Error(`Error in Fetch, Status: ${DBresponse.status}`);
+        }
+
+        const DBJsonData = await DBresponse.json();
+
+        setData(DBJsonData);
+      } catch (error) {
+        console.error("Fetch error:", error);
+      }
+    };
+
+    fetch_data();
+
+    const intervalId = setInterval(fetch_data, 500);
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <>
+      <div>{Data.userId}</div>
+      <div>{Data.id}</div>
+      <div>{Data.title}</div>
+    </>
+  );
+};
 
 export default DataDisplay;
